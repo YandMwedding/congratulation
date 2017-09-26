8580730fba62ed52ed55be25f1c049db88eafb05
IsseiT <issei.tsuchihashi@gmail.com>
2017-09-26 21:48:53 +0900

    ハズレページを追加

--- js/Question.js
+++ js/Question.js
@@ -1,15 +1,34 @@
+// あたりか？
+var _isLot = function() {
+    // #other-people: ハズレ
+    // #lucky: アタリ
+    return location.hash === '#lucky';
+}
+
 var Question = function() {};
 Question.prototype.getElements = function() {
-    var elemetns = [];
+    var elements = [];
     // 問題番号
     var $index = $('<img>').attr('src', './images/q_j.png');
-    elemetns.push($index);
+    elements.push($index);
 
     // 問題文
-    var $text = $('<h1>').text('新郎の下の名前は？');
-    elemetns.push($text);
+    if  (_isLot()) {
+        var $title = $('<h1>').text('問題'); 
+        var $text = $('<h1>').text('新郎の下の名前は？');
+        elements.push($title);
+        elements.push($text);
+    }　else {
+        var $losingMsg = $('<h1>').text('はずれ〜〜〜〜');
+        var $ghostImg = $('<img>').addClass('ghost').attr('src', './images/ghost.png');
+        var $otherMsg = $('<h1>').text('他の人の名札を確認しよう！');
 
-    return elemetns;
+        elements.push($losingMsg);
+        elements.push($ghostImg);
+        elements.push($otherMsg);
+    }
+
+    return elements;
 };
 
 window.Question = Question;
